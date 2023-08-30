aa2bc3fba50defeb0f7d19e7d49c30523baecf40
fabian57fabian <fabiangreavu@gmail.com>
2023-08-31 00:05:34 +0200

    Added network class

---
+++ src/prototypical_net.py
@@ -0,0 +1,26 @@
+import torch.nn as nn
+
+
+class PrototypicalNetwork(nn.Module):
+
+    def _conv_layer(self, input, output, kernel_size, padding):
+        return nn.Sequential(
+            nn.Conv2d(input, output, kernel_size, padding=padding),
+            nn.BatchNorm2d(output),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+    def __init__(self, input_units, hidden_units, output_units):
+        super(PrototypicalNetwork, self).__init__()
+        self.net = nn.Sequential(
+            self._conv_layer(input_units, hidden_units, 16, 1),
+            self._conv_layer(hidden_units, hidden_units, 8, 1),
+            self._conv_layer(hidden_units, hidden_units, 3, 1),
+            self._conv_layer(hidden_units, output_units, 3, 1),
+        )
+
+    def forward(self, x):
+        y = self.net(x)
+        return y.view(y.size(0), -1)
+
--- src/train.py
+++ src/train.py
@@ -1,9 +1,16 @@
 import os
 
+from prototypical_net import PrototypicalNetwork
+
 import numpy as np
 import torch
 print(torch.__version__)
 
 
 def train():
-    pass
+
+    device = 'cpu'
+    if torch.cuda.is_available():
+        device = 'cuda:0'
+
+    model = PrototypicalNetwork(1, 32, 32).to(device)
