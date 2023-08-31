c3c97150933904ed44b624457424338150e5dabc
fabian57fabian <fabiangreavu@gmail.com>
2023-08-31 22:53:13 +0200

    Added base dataloader for mini imagenet

--- src/train.py
+++ src/train.py
@@ -1,16 +1,25 @@
 import os
-
-from prototypical_net import PrototypicalNetwork
+from src.prototypical_net import PrototypicalNetwork
+from src.DatasetMiniImagenet import DatasetMiniImagenet
 
 import numpy as np
 import torch
 print(torch.__version__)
 
+# example trian from https://github.com/pytorch/examples/blob/main/mnist/main.py
 
-def train():
 
-    device = 'cpu'
+def train(epochs):
+    data_loader = DatasetMiniImagenet("../datasets/mini_imagenet", (28, 28))
+    device = torch.device("cpu")
     if torch.cuda.is_available():
-        device = 'cuda:0'
+        device = torch.device("cuda")
 
     model = PrototypicalNetwork(1, 32, 32).to(device)
+
+    for epoch in range(epochs):
+        print(f"Ep {epoch}")
+        model.train()
+        # Train
+        for batch in next(data_loader):
+            pass
