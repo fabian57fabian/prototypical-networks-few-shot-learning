82bac9c30e9a436979b64ec8f3b8cae3c83457d9
fabian57fabian <fabiangreavu@gmail.com>
2023-09-13 10:07:13 +0200

    Added train_all script

---
+++ train_all.sh
@@ -0,0 +1,22 @@
+
+
+
+python3 train.py --dataset mini_imagenet --number-support 5 --distance-function cosine --gpu
+
+python3 train.py --dataset flowers102 --image-size 74 --number-support 5 --distance-function cosine --gpu
+
+python3 train.py --dataset omniglot --number-support 5 --distance-function cosine --gpu
+
+
+python3 train.py --dataset mini_imagenet --number-support 5 --gpu
+
+python3 train.py --dataset flowers102 --image-size 74 --number-support 5 --gpu
+
+python3 train.py --dataset omniglot --number-support 5 --gpu
+
+
+python3 train.py --dataset mini_imagenet --number-support 1 --gpu
+
+python3 train.py --dataset flowers102 --image-size 74 --number-support 1 --gpu
+
+python3 train.py --dataset omniglot --number-support 1 --gpu
