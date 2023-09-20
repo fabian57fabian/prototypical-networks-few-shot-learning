c74934c0b8676b265174bee450e9b5b0912576a6
Fabian Greavu <fabiangreavu@gmail.com>
2023-09-20 16:49:42 +0200

    Removed torch gpu test, set general torch test

--- tests/test_prototypical_net.py
+++ tests/test_prototypical_net.py
@@ -7,8 +7,10 @@ from src.prototypical_net import PrototypicalNetwork
 
 
 class TestPrototypicalNetwork(TestCase):
-    def test_forward_cpu(self):
+    def test_forward_torch(self):
         device = torch.device("cpu")
+        if torch.cuda.is_available():
+            device = torch.device("cuda:0")
         model = PrototypicalNetwork().to(device)
         x = torch.rand(size=(600, 3, 84, 84)).to(device)
         def get_time():
@@ -16,25 +18,9 @@ class TestPrototypicalNetwork(TestCase):
             out = model(x)
             duration = (datetime.now() - start_time).total_seconds() * 1000
             return duration
-        durations = [get_time() for _ in range(5)]
-        mean = sum(durations) / len(durations)
-        print(f"GetSample avg CPU ms: {mean}, [{durations}]")
+        try:
+            durations = [get_time() for _ in range(5)]
+        except Exception as e:
+            print("Got errors while forwarding on torch")
+            assert False, "not runned on torch: " + str(e)
         assert True
-
-    def test_forward_gpu(self):
-        if not torch.cuda.is_available():
-            print("cuda not available for tests")
-            assert True
-        device = torch.device("cuda:0")
-        model = PrototypicalNetwork().to(device)
-        x = torch.rand(size=(600, 3, 84, 84)).to(device)
-        def get_time():
-            start_time = datetime.now()
-            out = model(x)
-            duration = (datetime.now() - start_time).total_seconds() * 1000
-            return duration
-        durations = [get_time() for _ in range(5)]
-        mean = sum(durations) / len(durations)
-        print(f"GetSample avg GPU ms: {mean}, [{durations}]")
-        assert True
-
